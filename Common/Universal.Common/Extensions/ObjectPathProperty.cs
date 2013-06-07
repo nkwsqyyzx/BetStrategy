7ee066ca704e925a189b90040a33a2ade4d642c9
nkwsqyyzx <nk.wangshuangquan@gmail.com>
2013-06-07 10:41:52 +0100

    add extension for get value through property path

---
+++ Common/Universal.Common/Extensions/ObjectPathProperty.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Reflection;
+
+namespace WSQ.CSharp.Extensions
+{
+    public static class ObjectPathProperty
+    {
+        /// <summary>
+        /// return the property with property path.
+        /// </summary>
+        /// <param name="o"></param>
+        /// <param name="path"></param>
+        /// <returns>proprety value</returns>
+        /// <exception cref="System.ArgumentException">PropertyPath is invalid.</exception>
+        /// <exception cref="System.NullReferenceException">the property path has null value.</exception> 
+        public static object Property(this object o, string PropertyPath)
+        {
+            if (string.IsNullOrEmpty(PropertyPath))
+            {
+                throw new System.ArgumentException("PropertyPath is invalid", "PropertyPath");
+            }
+            object result = null;
+            var ps = PropertyPath.Split('.');
+            Type type = o.GetType();
+            PropertyInfo info = null;
+            if (ps.Length == 1)
+            {
+                info = type.GetProperty(ps[0]);
+                result = info.GetValue(o, null);
+            }
+            else
+            {
+                result = o;
+                foreach (var pro in ps)
+                {
+                    info = type.GetProperty(pro);
+                    result = info.GetValue(result, null);
+                    type = result.GetType();
+                }
+            }
+            return result;
+        }
+    }
+}
---
+++ UnitTest/CommonTest/UnitTest3.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System.Threading;
+using WSQ.CSharp.Extensions;
+
+namespace CommonTest
+{
+    public class A
+    {
+        public string szA { get; set; }
+        public int iA { get; set; }
+    }
+
+    public class B
+    {
+        public A A { get; set; }
+
+        public string szB { get; set; }
+    }
+
+    [TestClass]
+    public class UnitTest3
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var m = (new B()).ComparerFromProperty("A.szA");
+            List<B> bs = new List<B>();
+            B b1 = new B() { A = new A() { szA = "szA1", iA = 10 }, szB = "szB1" };
+            B b2 = new B() { A = new A() { szA = "szA2" }, szB = "szB2" };
+            bs.Add(b1);
+            bs.Add(b2);
+
+            bs.Sort(m);
+
+            Thread.Sleep(100000);
+        }
+
+        [TestMethod]
+        public void EmptyTest()
+        {
+        }
+
+        [TestMethod]
+        public void TestProperty()
+        {
+            B b1 = new B() { A = new A() { szA = "szA1" }, szB = "szB1" };
+            var x = b1.Property("szB");
+            var y = b1.Property("A");
+            var z = b1.Property("A.szA");
+            var m = b1.Property("A.iA");
+        }
+    }
+}
