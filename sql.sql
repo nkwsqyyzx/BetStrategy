60348c054d621e4d1fb3bd81be6a4ba5f1894e7c
nkwsqyyzx <nk.wangshuangquan@gmail.com>
2013-06-14 18:15:47 +0800

    modify database

---
+++ sql.sql
@@ -0,0 +1,15 @@
+create table Recommends
+(
+Current varchar(50),
+League varchar(50),
+Host varchar(50),
+OddStake varchar(50),
+Guest varchar(50),
+Prefer varchar(50),
+Result varchar(50),
+Person varchar(50),
+Time1 datetime,
+Time2 datetime,
+Odds float,
+PreferResult int
+)
