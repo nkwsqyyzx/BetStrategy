3381e46c10faac575f2252842f16236d15af443b
nkwsqyyzx <nk.wangshuangquan@gmail.com>
2013-07-25 10:12:06 +0100

    delete unused file

--- BetStrategy/ViewModels/BaseViewModel.cs
+++ BetStrategy/ViewModels/BaseViewModel.cs
@@ -1,7 +1,19 @@
-﻿
+﻿using System;
+using System.ComponentModel;
+using System.Linq.Expressions;
+
 namespace BetStrategy.ViewModels
 {
-    public class BaseViewModel : NotifyModel
+    public class BaseViewModel : INotifyPropertyChanged
     {
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void NotifyPropertyChange<T>(Expression<Func<T>> expression)
+        {
+            if (PropertyChanged != null)
+            {
+                var propertyName = ((MemberExpression)expression.Body).Member.Name;
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
     }
 }
